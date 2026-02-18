ae41f6fea67d7c8e5a6965001cd6fc35a215a6db
Tina Taylor <longevity.tina@gmail.com>
2026-02-18 10:42:21 -0800

    update app settings

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,10 @@
 from app import create_app
+from app.config import Settings
 
 app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    settings = Settings()
+    app.run(debug=True, port=settings.flask_run_port)
 
